14674a2f3cd4a07c85b10df8ee76c6d87322e324
jonywu <jonywu@mitac.com.tw>
2023-12-12 19:16:51 +0800

    [ubike] 關鍵字搜尋完成，sort進行中

--- ubike/src/App.vue
+++ ubike/src/App.vue
@@ -1,72 +1,112 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+  import { ref, computed, watch } from 'vue';
 
-// 修改這份 YouBike 即時資訊表，並加上
-// 1. 站點名稱搜尋
-// 2. 目前可用車輛 / 總停車格 的排序功能
-// 3. 加入分頁功能，每頁 20 筆資料
+  // 修改這份 YouBike 即時資訊表，並加上
+  // 1. 站點名稱搜尋
+  // 2. 目前可用車輛 / 總停車格 的排序功能
+  // 3. 加入分頁功能，每頁 20 筆資料
 
-// 欄位說明:
-// https://data.taipei/dataset/detail?id=c6bc8aed-557d-41d5-bfb1-8da24f78f2fb
-// sno：站點代號、 sna：場站名稱(中文)、 tot：場站總停車格、
-// sbi：場站目前車輛數量、 sarea：場站區域(中文)、 mday：資料更新時間、
-// lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
-// snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
+  // 欄位說明:
+  // https://data.taipei/dataset/detail?id=c6bc8aed-557d-41d5-bfb1-8da24f78f2fb
+  // sno：站點代號、 sna：場站名稱(中文)、 tot：場站總停車格、
+  // sbi：場站目前車輛數量、 sarea：場站區域(中文)、 mday：資料更新時間、
+  // lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
+  // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
 
-const uBikeStops = ref([]);
+  const uBikeStops = ref([]);                           //接收資料庫站點資料
+  const searchInput = ref("");                          //接收使用者輸入值
+  const isAscending = ref(false);                       //是否升冪
 
-fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
-  .then(res => res.text())
-  .then(data => {
-    uBikeStops.value = JSON.parse(data);
-  });
+  fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
+    .then(res => res.text())
+    .then(data => {
+      uBikeStops.value = JSON.parse(data);
+    });
+  
+  const filUBikeStops = computed(()=>{                  //關鍵字篩選結果
+    return uBikeStops.value.filter((stop)=>{
+      return stop.sna.indexOf(searchInput.value) !== -1;
+    })
+  })
+  
+  // const sortFilUBikeStops = computed(()=>{  //篩選結果的資料排序
+  //               //篩選結果的資料排序
+  //   // if(filUBikeStops.value.length != 0){
+  //   //   let arr = Array.from(filUBikeStops.value);
+  //   // }
+  //   let arr = ref([]);
+  //   if (filUBikeStops.value.length != 0) {
+  //     arr = Array.from(filUBikeStops.value);
+  //   }
+    
 
-const timeFormat = (val) => {
-  // 時間格式
-  const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
-  return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
-};
+  //   arr = arr.slice().sort((a, b) => {
+  //     return a.sbi - b.sbi;
+  //   });
+  //   return arr;
+  //   // arr = arr.value.filter((p)=>{
+  //   //   return p.sbi === 3;
+  //   // })
+  //   // console.log(arr);
+    
+  // })
+
+  // const sort = (()=>{
+  //   filUBikeStops.value = filUBikeStops.value.slice().sort((a, b) => {
+  //     return a.sbi - b.sbi;
+  //   })
+  // })
+
+
+
+
+  const timeFormat = (val) => {
+    // 時間格式
+    const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
+    return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
+  };
 </script>
 
 <template>
-<div class="app">
-  <p>
-    站點名稱搜尋: <input type="text">
-  </p>
+  <div class="app">
+    <p>
+      站點名稱搜尋: <input type="text" v-model="searchInput">
+    </p>
+    
 
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>場站名稱</th>
-        <th>場站區域</th>
-        <th>目前可用車輛
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>總停車格
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>資料更新時間</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="s in uBikeStops" :key="s.sno">
-        <td>{{ s.sno }}</td>
-        <td>{{ s.sna }}</td>
-        <td>{{ s.sarea }}</td>
-        <td>{{ s.sbi }}</td>
-        <td>{{ s.tot }}</td>
-        <td>{{ timeFormat(s.mday) }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>場站名稱</th>
+          <th>場站區域</th>
+          <th @click="sort()" style="cursor:pointer;">目前可用車輛
+            <i class="fa fa-sort-asc" aria-hidden="true"></i>
+            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          </th>
+          <th  style="cursor:pointer;">總停車格
+            <i class="fa fa-sort-asc" aria-hidden="true"></i>
+            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          </th>
+          <th>資料更新時間</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in filUBikeStops" :key="s.sno">
+          <td>{{ s.sno }}</td>
+          <td>{{ s.sna }}</td>
+          <td>{{ s.sarea }}</td>
+          <td>{{ s.sbi }}</td>
+          <td>{{ s.tot }}</td>
+          <td>{{ timeFormat(s.mday) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
-.app {
-  padding: 1rem;
-}
+  .app {
+    padding: 1rem;
+  }
 </style>
