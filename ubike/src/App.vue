03d7425a71f8192f587b7258c920a8a93a927d87
jonywu <jonywu@mitac.com.tw>
2023-12-13 18:25:08 +0800

    [ubike] 關鍵字搜尋完成，sort進行中....好難

--- ubike/src/App.vue
+++ ubike/src/App.vue
@@ -14,9 +14,13 @@
   // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
   const temp = ref(false);
   const uBikeStops = ref([]);                           //接收資料庫站點資料
-  const searchInput = ref("");                          //接收使用者輸入值
+  const searchInput = ref("");  
+                          //接收使用者輸入值
   const sortBike = ref(false);                    //可用車輛sort被點擊                
-  const sortParking = ref(false);                     //總停車格sort被點擊
+  const sortParking = ref(false); 
+
+  const tableSelect = ref('');                    //總停車格sort被點擊
+  const tableCode = ref('');
 
   fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
     .then(res => res.text())
@@ -28,6 +32,7 @@
   const filUBikeStops = computed(()=>{    
     
     let arr = ref([]);
+    const tableCode = ref(tableCode.value);
     
     //篩選關鍵字
     arr = uBikeStops.value.filter((stop)=>{
@@ -35,36 +40,82 @@
     });
 
     //如果”可用車輛“被點擊
-    if (sortBike.value == true) {
-      arr = arr.slice().sort((a, b) => {
-        return a.sbi - b.sbi;
-      });
-    }
-    if (sortBike.value == false) {
-      arr = arr.slice().sort((a, b) => {
-        return b.sbi - a.sbi;
-      });
-    }
+    // if (sortBike.value == true) {
+    //   arr = arr.slice().sort((a, b) => {
+    //     return a.sbi - b.sbi;
+    //   });
+    // } else {
+    //   arr = arr.slice().sort((a, b) => {
+    //     return b.sbi - a.sbi;
+    //   });
+    // }
+
     
-    //如果“總停車格”被點擊
-    if (sortParking.value == true) {
-      arr = arr.slice().sort((a, b) => {
-        return a.tot - b.tot;
-      });
-    }
-    if (sortParking.value == false) {
-      arr = arr.slice().sort((a, b) => {
-        return b.tot - a.tot;
-      });
-    }
+    arr = arr.slice().sort((a, b) => {
+      return a[type] - b[type];
+    });
+    
 
     return arr;
   });
 
-  
-  
+  //搜集點擊的 table head
+  const selectTable = ((tableName) => {
+    tableSelect.value = tableName;
+    if (tableSelect.value == 'bikes') {
+      tableCode.value = 'sbi'
+    } else if (tableSelect.value == 'space') {
+      tableCode.value = 'tot'
+    }
+    console.log(tableSelect.value);
+  })
+
+  const sortResult = ((tableCode)=>{
+
+    if (condition) {
+      
+    }
+    
+      return direction === true ?
+      (a, b) => a[tableCode] - b[tableCode] :
+      (a, b) => b[tableCode] - c[tableCode]
+    
+  })
 
 
+
+  //如果”可用車輛“被點擊
+  // watch (sortBike, (value)=>{
+  //   console.log('watch',value);
+  //   if (value == true) {
+  //     filUBikeStops = filUBikeStops.value.slice().sort((a, b) => {
+  //       return a.sbi - b.sbi;
+  //     });
+  //   }
+  //   if (value == false) {
+  //     filUBikeStops = filUBikeStops.value.slice().sort((a, b) => {
+  //       return b.sbi - a.sbi;
+  //     });
+  //   }
+  // })
+
+  //如果“總停車格”被點擊
+  // watch(sortParking, (value) => {
+  //   console.log('watch', value);
+  //   if (value == true) {
+  //     filUBikeStops = filUBikeStops.value.slice().sort((a, b) => {
+  //       return a.tot - b.tot;
+  //     });
+  //   }
+  //   if (value == false) {
+  //     filUBikeStops = filUBikeStops.value.slice().sort((a, b) => {
+  //       return b.tot - a.tot;
+  //     });
+  //   }
+  // })
+
+  
+  
   const timeFormat = (val) => {               // 時間格式
     const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
     return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
@@ -83,11 +134,11 @@
           <th>#</th>
           <th>場站名稱</th>
           <th>場站區域</th>
-          <th @click="sortBike = !sortBike" style="cursor:pointer;">目前可用車輛
+          <th @click="selectTable('bikes')" style="cursor:pointer;">目前可用車輛
             <i class="fa fa-sort-asc" aria-hidden="true"></i>
             <i class="fa fa-sort-desc" aria-hidden="true"></i>
           </th>
-          <th @click="sortParking = !sortParking" style="cursor:pointer;">總停車格
+          <th @click="selectTable('space')" style="cursor:pointer;">總停車格
             <i class="fa fa-sort-asc" aria-hidden="true"></i>
             <i class="fa fa-sort-desc" aria-hidden="true"></i>
           </th>
