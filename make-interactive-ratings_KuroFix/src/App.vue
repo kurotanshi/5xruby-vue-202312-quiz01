3c6c5a4d14ba60b26c51dfaa8826d1a4ba56389a
jonywu <jonywu@mitac.com.tw>
2023-12-15 15:57:11 +0800

    [make-interactive-ratings] Kuro 修正版（後）

--- make-interactive-ratings_KuroFix/src/App.vue
+++ make-interactive-ratings_KuroFix/src/App.vue
@@ -1,81 +1,53 @@
 <script setup>
-  /** 
-  完成以下功能：
-  1. 將 movies 的內容用 v-for 改寫成動態渲染
-  2. 完成電影星星評分的事件綁定，同樣用 v-for 進行改寫
-  */
-  import { ref } from "vue";
-  import { StarIcon } from "@heroicons/vue/24/solid";
-  import { items } from "./movies.json";
-
-  const movies = ref(items);                                        //電影正本
-  
-  const copyMovies = ref(JSON.parse(JSON.stringify(movies.value))); //電影副本
-  console.log(copyMovies.value[0]); 
-
-  copyMovies.value.forEach(element => {                             //電影副本添加屬性starHover
-    element.starHover = 0;
-    console.log('foreach element', element.starHover);
-  });
-
-  const mouseover = ((moviesIndex, star) => {                       //hover:
-    console.log('[hover]星星數',star);
-    console.log('[hover]電影資料 array index', moviesIndex - 1);
-    copyMovies.value[moviesIndex - 1].starHover = star;
-    console.log("[hover]星星顯示數", copyMovies.value[moviesIndex - 1].starHover);
-  })
-  const mouseout = ((moviesIndex) => {                              //unHover:
-    copyMovies.value[0].starHover = 0;
-  })
-  const changeRating = ((moviesIndex, star) => {
-    console.log('[點擊]星星數', star);
-    console.log('[點擊]電影資料 array index',moviesIndex -1);
-    copyMovies.value[moviesIndex - 1].rating = star;
-    console.log('[點擊]電影評分',movies.value[moviesIndex - 1].rating);
-  })
+/**
+完成以下功能：
+1. 將 movies 的內容用 v-for 改寫成動態渲染
+2. 完成電影星星評分的事件綁定，同樣用 v-for 進行改寫
+*/
+import { ref } from 'vue';
+import { StarIcon } from '@heroicons/vue/24/solid';
+import { items } from './movies.json';
+const movies = ref(items);
+movies.value.forEach((m) => (m.tempRating = m.rating));
+const setRating = (i, n) => (movies.value[i].rating = n);
 </script>
 
 <template>
-<div class="app">
-  <!--v-for?star狀態-->
-  <div class="movie-list">
-    <div class="movie-item" v-for="(movie) in copyMovies" :key="movie.id">
-      <div class="movie-item-image-wrapper">
-        <img :src="movie.image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">{{movie.name}}</h3>
-          <div class="movie-item-genres-wrapper">
-            <template v-for="genre in movie.genres" :key="genre">
-              <span class="movie-item-genre-tag" >{{genre}}</span>
-            </template>
+  <div class="app">
+    <div class="movie-list">
+      <div class="movie-item" v-for="(m, index) in movies" :key="m.id">
+        <div class="movie-item-image-wrapper">
+          <img :src="m.image" class="movie-item-image" alt="" />
+        </div>
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">{{ m.name }}</h3>
+            <div class="movie-item-genres-wrapper">
+              <span class="movie-item-genre-tag" v-for="g in m.genres">
+                {{ g }}
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">{{movie.description}}</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: ({{movie.rating}}/5) </span>
-          
-          <div class="movie-item-star-icon-wrapper">
-              <button class="movie-item-star-icon-button" 
-                      v-for="(star) in 5" 
-                      :key="star" 
-                      :class="[
-                        star <= movie.rating ? 'text-yellow-500' : 'text-gray-500', 
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">{{ m.description }}</p>
+          </div>
+          <div class="movie-item-rating-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ m.rating }}/5)
+            </span>
 
-                        star <= movie.starHover ? 'text-yellow-500' : 'text-gray-500'
-                      ]" 
-                      @click="changeRating(movie.id, star)"
-                      @mouseover="mouseover(movie.id, star)"
-                      @mouseout="mouseout(movie.id)">
-                <StarIcon class="movie-item-star-icon"/>
-              </button>
+            <div class="movie-item-star-icon-wrapper">
+              <template v-for="(n, i) in 5">
+                <button class=" movie-item-star-icon-button"
+                  :class="m.tempRating > i ? 'text-yellow-500' : 'text-black-500'" @mouseover="m.tempRating = i + 1"
+                  @mouseout="m.tempRating = m.rating" @click="setRating(index, n)">
+                  <StarIcon class="movie-item-star-icon" />
+                </button>
+              </template>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
